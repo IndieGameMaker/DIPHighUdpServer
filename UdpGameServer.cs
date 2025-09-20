ef73369712816141e9bc8fef7002114165980b11
Lee JaeHyun <myleje@gmail.com>
2025-09-20 16:23:48 +0900

    feat: Receive , ClientInfo 클래스 정의

--- UdpGameServer.cs
+++ UdpGameServer.cs
@@ -6,6 +6,39 @@ using System.Text;
 
 namespace HighUDPServer;
 
+// 수신된 데이터 패킷을 저장하는 구조체
+public struct ReceivedData
+{
+    public byte[] Buffer { get; set; }              // 수신 데이터 버퍼
+    public int Length { get; set; }                 // 실제로 수신된 데이터 길이
+    public IPEndPoint ClientEndPoint { get; set; }  // 수신한 클라이언트 정보
+}
+
+// 클라이언트 정보를 저장하는 클래스
+public class ClientInfo
+{
+    public string playerId { get; set; } = string.Empty;    // Player ID
+    public IPEndPoint EndPoint { get; set; } = null;        // 클라이언트 EndPoint
+    public DateTime LastHeartbeat { get; set; } = DateTime.UtcNow;  // 마지막 하트비트 시간(연결상태 확인용)
+    public Vector3 Position { get; set; } = new Vector3();  // 플레이어의 좌표
+    public Vector3 Rotation { get; set; } = new Vector3();  // 플레이어의 회전
+}
+
+// 3D 벡터 데이터
+public struct Vector3
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+    public float Z { get; set; }
+
+    public Vector3(float x = 0, float y = 0, float z = 0)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
+
 public class UdpGameServer
 {
     private readonly Socket _socket;                // UDP 통신을 위한 소켓 객체
@@ -31,3 +64,5 @@ public class UdpGameServer
     // 서버 포트 번호
     private readonly int _port;
 }
+
+
